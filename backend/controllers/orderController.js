a200ba0f1ffec5832782cdc1c1904e4bcc143f67
Helvin Rymer <Helvin159@gmail.com>
2021-07-27 10:05:39 -0400

    Paypal, orderJson, orderPay reducer, orderPay constants, paypalscript appended dynamically, new functions

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -4,7 +4,6 @@ import Order from "../models/orderModel.js";
 // @desc Create New Order
 // @route POST /api/orders
 // @access Private
-
 const addOrderItems = asyncHandler(async (req, res) => {
 	const {
 		orderItems,
@@ -38,4 +37,47 @@ const addOrderItems = asyncHandler(async (req, res) => {
 	}
 });
 
-export { addOrderItems };
+// @desc Get an order by ID
+// @route GET /api/orders/:id
+// @access Private
+const getOrderById = asyncHandler(async (req, res) => {
+	const order = await Order.findById(req.params.id).populate(
+		"user",
+		"name email"
+	);
+
+	if (order) {
+		res.json(order);
+	}
+	else {
+		res.status(404);
+		throw new Error("Order not found");
+	}
+});
+
+// @desc Update order to PAID
+// @route GET /api/orders/:id/pay
+// @access Private
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+	const order = await Order.findById(req.params.id);
+
+	if (order) {
+		order.isPaid = true;
+		paidAt = Date.now();
+		order.paymentResult = {
+			id            : req.body.id,
+			status        : req.body.status,
+			update_time   : req.body.update_time,
+			email_address : req.body.payer.email_address,
+		};
+
+		const updatedOrder = await order.save();
+		res.json(updatedOrder);
+	}
+	else {
+		res.status(404);
+		throw new Error("Order not found");
+	}
+});
+
+export { addOrderItems, getOrderById, updateOrderToPaid };
